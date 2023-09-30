734dd08b5f60c66b02dc5516ed139f6ec7ffbbb8
anatoliy.ornatovskyy <anatoliy.ornatovskyy@bullhorn.com>
2023-09-30 21:03:43 +0200

    Routing and bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,15 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { AuthComponent } from './auth/auth.component';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+const routers: Routes = [
+  // {path: '', component: AppComponent},
+  {path: 'auth', component: AuthComponent}
+]
+
 
 @NgModule({
   declarations: [
@@ -19,6 +28,9 @@ import { AuthComponent } from './auth/auth.component';
     MatButtonModule,
     MatIconModule,
     MatToolbarModule,
+    MatInputModule,
+    FormsModule,
+    RouterModule.forRoot(routers)
   ],
   providers: [],
   bootstrap: [AppComponent]
